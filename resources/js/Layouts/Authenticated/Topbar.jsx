d9ee729989d8a3457c73aa770d901b85b15054cc
Muhammad Rayhan Gibran <mraygbrn@gmail.com>
2024-06-19 13:58:50 +0700

    feat(dashboard): integration dashboard

--- resources/js/Layouts/Authenticated/Topbar.jsx
+++ resources/js/Layouts/Authenticated/Topbar.jsx
@@ -1,6 +1,7 @@
 import { useRef, useState } from "react";
+import { Link } from "@inertiajs/react";
 
-export default function TopBar() {
+export default function TopBar({ name }) {
     const [dropdownOpen, setdropdownOpen] = useState(true);
     const dropdownTarget = useRef();
 
@@ -22,7 +23,7 @@ export default function TopBar() {
             />
             <div className="flex items-center gap-4">
                 <span className="text-black text-sm font-medium">
-                    Welcome, Granola Sky
+                    Welcome, {name}
                 </span>
                 <div className="collapsible-dropdown flex flex-col gap-2 relative">
                     <div
@@ -51,12 +52,13 @@ export default function TopBar() {
                         >
                             Settings
                         </a>
-                        <a
-                            href="sign_in.html"
+                        <Link
+                            href={route("logout")}
                             className="transition-all hover:bg-sky-100 p-4"
+                            method="post"
                         >
                             Sign Out
-                        </a>
+                        </Link>
                     </div>
                 </div>
             </div>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\User\DashboardController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -25,9 +26,9 @@ use Inertia\Inertia;
 
 Route::redirect('/', '/login');
 
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth'])->name('dashboard');
+Route::middleware(['auth', 'role:user'])->prefix('dashboard')->name('user.dashboard')->group(function () {
+    Route::get('/', [DashboardController::class, 'index']);
+});
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
